e4fec16632e011f746917f311af2cfcd1be86eda
KING-USER1 <82299966+KING-USER1@users.noreply.github.com>
2021-05-09 18:58:43 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -9,7 +9,7 @@ from telethon import Button, custom, events
 
 from . import CMD_LIST, catalive
 
-CAT_IMG = Config.ALIVE_PIC or None
+SKY_IMG = Config.ALIVE_PIC or None
 BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)\]\<buttonurl:(?:/{0,2})(.+?)(:same)?\>)")
 
 if Config.TG_BOT_USERNAME is not None and tgbot is not None:
@@ -25,26 +25,26 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
             buttons = [
                 (
                     custom.Button.inline("Stats", data="stats"),
-                    Button.url("Repo", "https://github.com/sandy1709/catuserbot"),
+                    Button.url("Repo", "https://github.com/KING-USER1/SkyHawk"),
                 )
             ]
             if CAT_IMG and CAT_IMG.endswith((".jpg", ".png")):
                 result = builder.photo(
                     CAT_IMG,
-                    # title="Alive cat",
+                    # title="Alive skyhawk",
                     text=query,
                     buttons=buttons,
                 )
             elif CAT_IMG:
                 result = builder.document(
                     CAT_IMG,
-                    title="Alive cat",
+                    title="Alive skyhawk",
                     text=query,
                     buttons=buttons,
                 )
             else:
                 result = builder.article(
-                    title="Alive cat",
+                    title="Alive skyhawk",
                     text=query,
                     buttons=buttons,
                 )
@@ -237,7 +237,7 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
                         caption=plugin_name,
                     )
         else:
-            reply_pop_up_alert = "Please get your own catuserbot, and don't use mine! Join @catuserbot17 help "
+            reply_pop_up_alert = "Please get your own skyhawkbot, and don't use mine! Join @SKYHAWK_SUPPORT_GROUP help "
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
@@ -245,7 +245,7 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
         if event.query.user_id == bot.uid:
             await event.edit("menu closed")
         else:
-            reply_pop_up_alert = "Please get your own catuserbot, and don't use mine! Join @catuserbot17 help "
+            reply_pop_up_alert = "Please get your own skyhawkbot, and don't use mine! Join @SKYHAWK_SUPPORT_GROUP help "
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"stats")))
